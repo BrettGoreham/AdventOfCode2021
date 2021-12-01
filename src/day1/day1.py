a9412d0845e717b4033c9dbb2cdc22f07eaa08b0
Brett Goreham <brett.goreham@bouvet.no>
2021-12-01 07:46:28 +0100

    day 1 solutions

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -1,6 +1,31 @@
 with open('day1Input.txt') as f:
     content = f.readlines()
-content = [x.strip() for x in content]
+content = [int(x.strip()) for x in content]
 
+count = 0
+prev = None
 for c in content:
-    print(c)
+    if prev is None:
+        prev = c
+    else:
+        if c > prev:
+            count += 1
+        prev = c
+
+print('part 1', count)
+
+count2= 0
+prev = None
+for x in range(len(content)-2):
+    curr = content[x] + content[x+1] + content[x+2]
+    print(str(curr) + ' ' + str(prev))
+    if prev is not None and curr > prev:
+        print('true')
+        count2 += 1
+
+    prev = curr
+
+print(count2)
+
+
+
