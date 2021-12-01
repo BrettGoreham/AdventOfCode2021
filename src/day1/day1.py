71836bb4d080267ac6b4aede4b2318293a95d81a
Brett Goreham <brett.goreham@bouvet.no>
2021-12-01 12:43:42 +0100

    getting day 2 ready ahead of time

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -2,19 +2,18 @@ with open('day1Input.txt') as f:
     content = f.readlines()
 content = [int(x.strip()) for x in content]
 
+
 count = 0
 prev = None
 for c in content:
-    if prev is None:
-        prev = c
-    else:
-        if c > prev:
-            count += 1
-        prev = c
+    curr = c
+    if prev is not None and curr > prev:
+        count += 1
+    prev = curr
 
 print('part 1', count)
 
-count2= 0
+count2 = 0
 prev = None
 for x in range(len(content)-2):
     curr = content[x] + content[x+1] + content[x+2]
@@ -26,4 +25,21 @@ for x in range(len(content)-2):
 print('part 2', count2)
 
 
+#did this after the fact as a way to do it without repeating so much code
+def count_increasing_total(ints, look_ahead):
+    _count = 0
+    _prev = None
+    for x in range(len(ints) - (look_ahead - 1)):
+        _curr = 0
+        for i in range(look_ahead):
+            _curr += ints[x + i]
 
+        if _prev is not None and _curr > _prev:
+            _count += 1
+        _prev = _curr
+
+    return _count
+
+
+print('part 1 again', count_increasing_total(content, 1))
+print('part 2 again', count_increasing_total(content, 3))
---
+++ src/day2/day2.py
@@ -0,0 +1,4 @@
+with open('day2Input.txt') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
