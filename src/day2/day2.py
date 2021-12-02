0da22786b376850d05824118de495669e5fdd53c
Brett Goreham <brettgoreham@gmail.com>
2021-12-02 08:41:57 +0100

    day 2 6minute solution

--- src/day2/day2.py
+++ src/day2/day2.py
@@ -2,3 +2,33 @@ with open('day2Input.txt') as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
+
+x = 0
+y = 0
+for i in content:
+    part = i.split(' ')
+    if part[0] == 'forward':
+        x += int(part[1])
+    elif part[0] == 'down':
+        y += int(part[1])
+    elif part[0] == 'up':
+        y -= int(part[1])
+
+print('part 1', (x * y))
+
+
+x = 0
+y = 0
+aim = 0
+for i in content:
+    part = i.split(' ')
+    if part[0] == 'forward':
+        x += int(part[1])
+        y += aim * int(part[1])
+    elif part[0] == 'down':
+        aim += int(part[1])
+    elif part[0] == 'up':
+        aim -= int(part[1])
+
+
+print('part 2', (x * y))
---
+++ src/day3/day3.py
@@ -0,0 +1,4 @@
+with open('day3Input.txt') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
