2f4db8d1bb64708234fd985a1bfe825e47c33400
Brett Goreham <brettgoreham@gmail.com>
2021-12-04 12:58:16 +0100

    little cleanup and using none instead of empty string for already called numbers

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -2,14 +2,14 @@ def check_board(_board):
     for x in range(5):
         bingo = True
         for y in range(5):
-            if _board[x][y] != '':
+            if _board[x][y] is not None:
                 bingo = False
         if bingo:
             return bingo
     for y in range(5):
         bingo = True
         for x in range(5):
-            if _board[x][y] != '':
+            if _board[x][y] is not None:
                 bingo = False
         if bingo:
             return bingo
@@ -20,7 +20,7 @@ def find_number(_board, number):
     for x in range(5):
         for y in range(5):
             if _board[x][y] == number:
-                _board[x][y] = ''
+                _board[x][y] = None
                 return True
     return False
 
@@ -29,7 +29,7 @@ def count_remaining_numbers(_board):
     count = 0
     for x in range(5):
         for y in range(5):
-            if _board[x][y] != '':
+            if _board[x][y] is not None:
                 count += int(_board[x][y])
 
     return count
@@ -53,32 +53,30 @@ for i in range(2, len(content)-1, 6):
 numCount = 0
 firstBingo = True
 lastDrawnNumberForFirstBingo = -1
-
 lastDrawnNumberForLastBingo = -1
 
-boardsThatHaveWon = []
-while len(boardsThatHaveWon) < len(boards):
+finishedBoardIndex = []
+while len(finishedBoardIndex) < len(boards):
     num = numbersDrawn[numCount]
     for i in range(len(boards)):
-        if i in boardsThatHaveWon:
+        if i in finishedBoardIndex:
             continue
 
         found = find_number(boards[i], num)
         if found:
             complete = check_board(boards[i])
             if complete:
-                boardsThatHaveWon.append(i)
+                finishedBoardIndex.append(i)
                 if firstBingo:
                     lastDrawnNumberForFirstBingo = num
                     firstBingo = False
 
                 lastDrawnNumberForLastBingo = num
-                lastBoardCompleted = i
 
     numCount += 1
 
-firstWinCount = count_remaining_numbers(boards[boardsThatHaveWon[0]])
+firstWinCount = count_remaining_numbers(boards[finishedBoardIndex[0]])
 print('part1 ', firstWinCount * int(lastDrawnNumberForFirstBingo))
 
-lastWinCount = count_remaining_numbers(boards[boardsThatHaveWon[len(boardsThatHaveWon) - 1]])
+lastWinCount = count_remaining_numbers(boards[finishedBoardIndex[len(finishedBoardIndex) - 1]])
 print('part2 ', lastWinCount * int(lastDrawnNumberForLastBingo))
