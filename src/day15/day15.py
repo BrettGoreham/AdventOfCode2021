16a25cc49e3d759406723bc6073effcbb350ed97
Brett Goreham <brett.goreham@bouvet.no>
2021-12-15 15:16:14 +0100

    Addding comments explaining my madness

--- src/day15/day15.py
+++ src/day15/day15.py
@@ -48,14 +48,17 @@ def find_fastest_path(_map, _goal):
         path = paths.pop(0)
         point = path[0]
         value = path[1]
+        #if path was added before goal was found but value is greater than goals current skip path and move on
         if value < lowest_to_point.get(_goal, float('inf')):
             for vector in vectors:
                 new_point = (point[0] + vector[0], point[1] + vector[1])
                 if 0 <= new_point[0] < len(_map) and 0 <= new_point[1] < len(_map[0]):
                     new_point_value = value + _map[new_point[0]][new_point[1]]
+                    # if new point is not the lowest value for that point already found current path cant be the fastest.
                     if new_point_value < lowest_to_point.get(new_point, float('inf')):
                         # no point in keeping going if already greater
                         if new_point_value < lowest_to_point.get(_goal, float('inf')):
+                            # sorted inserting to ensure that the next path to check is the lowest value.
                             paths = sorted_insert(paths, [new_point, new_point_value])
                             lowest_to_point[new_point] = new_point_value
 
